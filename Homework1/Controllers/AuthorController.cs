a74081c4739466d36a392e0da0d9b078b417db21
Administrator <Administrator@WIN-EVTSC56461J>
2019-12-14 22:46:13 -0800

    Added_Create_Edit_Delete_For_Author_Book_Entities

--- Homework1/Controllers/AuthorController.cs
+++ Homework1/Controllers/AuthorController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace Homework1.Controllers
 {
@@ -17,6 +18,68 @@ namespace Homework1.Controllers
 
                 return View(authors);
             }
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Author author)
+        {
+            using (Model1 db = new Model1())
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Authors.Add(author);
+                    db.SaveChanges();
+                }
+                else return View(author);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            using (Model1 db = new Model1())
+            {
+                var author = db.Authors.Find(id);
+
+                return View(author);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Author author)
+        {
+            using (Model1 db = new Model1())
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(author).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                else return View(author);
+            }
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Delete(int id)
+        {
+            using (Model1 db = new Model1())
+            {
+                var author = db.Authors.Find(id);
+                if (author != null)
+                {
+                    db.Authors.Remove(author);
+                    db.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index");
         }
     }
 }
--- Homework1/Controllers/BookController.cs
+++ Homework1/Controllers/BookController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace Homework1.Controllers
 {
@@ -18,5 +19,73 @@ namespace Homework1.Controllers
                 return View(books);
             }
         }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            using (Model1 db = new Model1())
+            {
+                ViewBag.AuthorList = new SelectList(db.Authors.ToList(), "id", "LastName");
+
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Create(Book book)
+        {
+            using (Model1 db = new Model1())
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Books.Add(book);
+                    db.SaveChanges();
+                }
+                else return View(book);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            using (Model1 db = new Model1())
+            {
+                var book = db.Books.Find(id);
+                ViewBag.AuthorList = new SelectList(db.Authors.ToList(), "Id", "LastName");
+
+                return View(book);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Book book)
+        {
+            using (Model1 db = new Model1())
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(book).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                else return View(book);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            using (Model1 db = new Model1())
+            {
+                var book = db.Books.Find(id);
+                if (book != null)
+                {
+                    db.Books.Remove(book);
+                    db.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
     }
 }
